ec4515bad388cfb13c945fafb55736dd6b04bf47
mHuzefa <huzi6338@gmail.com>
2020-07-04 22:45:38 +0500

    Template added

--- CustomerManager/accounts/views.py
+++ CustomerManager/accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+
+def home(request):
+    return render(request, 'accounts/dashboard.html')
+
+def products(request):
+    return render(request, 'accounts/products.html')
+
+def customers(request):
+    return render(request, 'accounts/profiles.html')
