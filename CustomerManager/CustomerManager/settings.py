5aaae32285ad12f925fbfdded511cffef031293f
mHuzefa <huzi6338@gmail.com>
2020-07-05 16:38:34 +0500

    Database Connected #1

--- CustomerManager/CustomerManager/settings.py
+++ CustomerManager/CustomerManager/settings.py
@@ -118,4 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    "E:/Django/Django/CustomerManager/static",
+]
+
 STATIC_URL = '/static/'
--- CustomerManager/accounts/views.py
+++ CustomerManager/accounts/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
-
+from .models import *
 from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+    total_customers = Customer.objects.count()
+    total_orders = Order.objects.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+    context = {'orders': orders, 'customers': customers, 'total_customers': total_customers, 'total_orders': total_orders,
+               'delivered': delivered, 'pending': pending
+               }
+
+    return render(request, 'accounts/dashboard.html', context)
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'accounts/products.html', context)
 
 def customers(request):
     return render(request, 'accounts/profiles.html')
