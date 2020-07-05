32b61742e28d1e82b7b76378179d6a71e44570b5
mHuzefa <huzi6338@gmail.com>
2020-07-05 21:53:39 +0500

    CRUD Operations

--- CustomerManager/accounts/urls.py
+++ CustomerManager/accounts/urls.py
@@ -2,7 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('products/', views.products),
-    path('customers/', views.customers),
-]
+    path('', views.home, name='home'),
+    path('products/', views.products, name='products'),
+    path('customers/<str:pk>/', views.customers, name='customers'),
+    path('create-order/', views.createOrder, name='create-order'),
+    path('create-customer/', views.AddCustomer, name='create-customer'),
+    path('update-order/<str:pk>/', views.updateOrder, name='update-order'),
+    path('delete-order/<str:pk>/', views.deleteOrder, name='delete-order'),
+    path('update-customer/<str:pk>', views.updateCustomer, name='update-customer'),
+    path('delete-customer/<str:pk>/', views.deleteCustomer, name='delete-customer'),
+    ]
--- CustomerManager/accounts/views.py
+++ CustomerManager/accounts/views.py
@@ -1,25 +1,98 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 from django.http import HttpResponse
-
-
+from .forms import *
 def home(request):
     orders = Order.objects.all()
     customers = Customer.objects.all()
     total_customers = Customer.objects.count()
     total_orders = Order.objects.count()
+
     delivered = orders.filter(status='Delivered').count()
     pending = orders.filter(status='Pending').count()
     context = {'orders': orders, 'customers': customers, 'total_customers': total_customers, 'total_orders': total_orders,
-               'delivered': delivered, 'pending': pending
+               'delivered': delivered, 'pending': pending,
                }
 
     return render(request, 'accounts/dashboard.html', context)
+
 
 def products(request):
     products = Product.objects.all()
     context = {'products': products}
     return render(request, 'accounts/products.html', context)
 
-def customers(request):
-    return render(request, 'accounts/profiles.html')
+def customers(request, pk):
+    customer = Customer.objects.get(id=pk)
+    order = customer.order_set.all()
+    total_order = customer.order_set.all()
+    order_count = total_order.count()
+    context = {'customer': customer, 'order': order, 'order_count': order_count}
+    return render(request, 'accounts/profiles.html', context)
+
+
+def createOrder(request):
+
+    form = OrderForm()
+    context = {'form':form}
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    return render(request, 'accounts/OrderForm.html', context)
+
+
+def updateOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    form = OrderForm(instance=order)
+    context = {'form': form}
+    if request.method == 'POST':
+        form = OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    return render(request, 'accounts/OrderForm.html', context)
+
+def deleteOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    if request.method == 'POST':
+        order.delete()
+        return redirect('/')
+    context = {'item': order}
+    return render(request, 'accounts/delete.html', context)
+
+
+
+def AddCustomer(request,):
+    Cform = CustomerForm()
+    context={'Cform': Cform}
+
+    if request.method == 'POST':
+        Cform = CustomerForm(request.POST)
+        if Cform.is_valid():
+            Cform.save()
+            return redirect('/')
+    return render(request,'accounts/CustomerForm.html', context)
+
+def updateCustomer(request, pk):
+    customer = Customer.objects.get(id=pk)
+    form = CustomerForm(instance=customer)
+    context = {'Cform': form}
+    if request.method == 'POST':
+        form = CustomerForm(request.POST, instance=customer)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    return render(request, 'accounts/CustomerForm.html', context)
+
+
+def deleteCustomer(request, pk):
+    customer = Customer.objects.get(id=pk)
+    if request.method == 'POST':
+        customer.delete()
+        return redirect('/')
+    context = {'customer': customer}
+    return render(request, 'accounts/deleteCustomer.html', context)
